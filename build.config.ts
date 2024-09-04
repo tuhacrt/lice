28b71082c78d0120878712071f9b93ae7d517150
Tuhacrt <a067800497@gmail.com>
2024-09-04 11:54:04 +0800

    chore: revise package.json exports, bin

--- build.config.ts
+++ build.config.ts
@@ -11,30 +11,3 @@ export default defineBuildConfig({
   declaration: true,
   clean: true,
 })
-
-/*
-import { defineBuildConfig } from 'unbuild'
-
-export default defineBuildConfig({
-  entries: [
-    'src/index',
-    'src/cli',
-  ],
-  rollup: {
-    inlineDependencies: true,
-    json: {
-      compact: true,
-      namedExports: false,
-      preferConst: false,
-    },
-    commonjs: {
-      requireReturnsDefault: 'auto',
-    },
-    dts: {
-      respectExternal: false,
-    },
-  },
-  clean: true,
-  declaration: true,
-})
-*/
