7424262f5c98950d9d1371bd86ee6486a140f12c
Tuhacrt <a067800497@gmail.com>
2024-09-04 16:22:18 +0800

    feat: use yargs to operate as a cli tool

--- src/lice.ts
+++ src/lice.ts
@@ -1,4 +1,11 @@
+import { join } from 'node:path'
+import process from 'node:process'
+
 import { init } from 'license-checker-rseidelsohn'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import pkgJson from '../package.json'
 
 import { getManyDependencyInformation } from './utils/getManyDependencyInformation'
 import { getManyUniqueDependencyNames } from './utils/getManyUniqueDependencyNames'
@@ -6,7 +13,7 @@ import { getPackageInformationMap } from './utils/getPackageInformationMap'
 import { readPackageJsonFile } from './utils/readPackageJsonFile'
 import { writeJsonFile } from './utils/writeJsonFile'
 
-export function lice(start: string, outputFilePath: string, outputFileName: string): void {
+export function lice(start: string, outputFilePath: string, outputFileName?: string): void {
   init(
     {
       start,
@@ -21,7 +28,7 @@ export function lice(start: string, outputFilePath: string, outputFileName: stri
         licenseModified: '',
       },
     },
-    (err, moduleInfos) => {
+    async (err, moduleInfos) => {
       if (err) {
         throw err
       }
@@ -30,12 +37,43 @@ export function lice(start: string, outputFilePath: string, outputFileName: stri
         throw new Error('please specify where to create the file as third argument')
       }
 
+      const startPkg = await import(join(process.cwd(), start, 'package.json'))
+      const defaultOutputFileName = `${startPkg.name}@${startPkg.version}.json`
+
       const packageMap = getPackageInformationMap(moduleInfos)
       const dependencies = readPackageJsonFile(start)
       const uniqueDependencyNames = getManyUniqueDependencyNames(dependencies)
       const directDevDependencies = getManyDependencyInformation(packageMap, uniqueDependencyNames)
 
-      return writeJsonFile(outputFilePath, directDevDependencies, outputFileName)
+      return writeJsonFile(outputFilePath, directDevDependencies, outputFileName || defaultOutputFileName)
     },
   )
 }
+
+const argv = yargs(hideBin(process.argv))
+  .scriptName('lice')
+  .usage('Usage: $0 -i <inputFilePath> -o <outputFilePath> -n <outputFileName>')
+  .option('inputFilePath', {
+    alias: 'i',
+    type: 'string',
+    describe: 'the folder path of target package.json',
+  })
+  .option('outputFilePath', {
+    alias: 'o',
+    type: 'string',
+    describe: 'the path to create the output file',
+  })
+  .option('outputFileName', {
+    alias: 'n',
+    type: 'string',
+    describe: 'the name of the output file, defaults to package "{name}@{version}.json"',
+  })
+  .demandOption(['inputFilePath', 'outputFilePath'])
+  .showHelpOnFail(false)
+  .alias('h', 'help')
+  .version('version', pkgJson.version)
+  .alias('v', 'version')
+  .argv
+
+// @ts-expect-error - TS doesn't know that yargs will populate argv
+lice(argv.inputFilePath, argv.outputFilePath, argv.outputFileName)
